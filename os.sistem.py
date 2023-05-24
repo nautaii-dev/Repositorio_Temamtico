76854c8a77bb7f473105168b1bdcffa42397e7f0
Michael Dev <128884673+nautaii-dev@users.noreply.github.com>
2023-05-24 16:55:44 -0400

    Actualizar os.sistem.py

--- os.sistem.py
+++ os.sistem.py
@@ -1,11 +1,13 @@
 import os
+
 def mostrar_capacidad_sistema():
     statvfs = os.statvfs("/")
     capacidad_total = statvfs.f_frsize * statvfs.f_blocks
     capacidad_libre = statvfs.f_frsize * statvfs.f_bfree
     capacidad_usada = capacidad_total - capacidad_libre
+
     print("Capacidad total del sistema:", capacidad_total)
     print("Capacidad libre del sistema:", capacidad_libre)
     print("Capacidad usada del sistema:", capacidad_usada)
-mostrar_capacidad_sistema()
 
+mostrar_capacidad_sistema()
